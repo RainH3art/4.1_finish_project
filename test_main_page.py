1775d9c37e182179da546f3439961c828e5713db
rainh3art <yusovqa@gmail.com>
2023-12-17 00:20:29 +0300

    Забыл убрать тестовый кейс - 8 шаг исправил

--- test_main_page.py
+++ test_main_page.py
@@ -13,10 +13,5 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
     
-def test_guest_should_see_register_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
     
     
